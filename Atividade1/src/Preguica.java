b3b136f0b7515a3bac2541a2fec97faf5b640f72
Hiran Kevin <117870293+HiranKDS@users.noreply.github.com>
2023-04-29 15:34:44 -0300

    Atividade_1

---
+++ Atividade1/src/Preguica.java
@@ -0,0 +1,10 @@
+public class Preguica extends Animal {
+
+    public Preguica(){
+        System.out.println("É uma Preguiça");
+    }
+    @Override
+    public void emitirSom(){
+        System.out.println("AAAAAAAAHHHHZZZ");
+    }
+}
